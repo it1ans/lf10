d66cef10f7c34fa2f19715c75ffcb5587bbe6dfb
phannemann <phannemann@mindworks.de>
2023-09-01 09:14:49 +0200

    #6: fix error in HomeController.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     {
         
         return $this->render('home/index.html.twig', [
-            'user' => $user
+            
         ]);
     }
 }
