9791b3748c80602022415c37e8371a4a285cc47f
p-hannemann <pascal.hannemann@gmail.com>
2023-09-05 13:08:45 +0200

    Bootstrap hinzufügen Fixes #16

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,7 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        
         return $this->render('home/index.html.twig', [
-            
         ]);
     }
 }
